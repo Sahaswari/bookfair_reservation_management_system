f26d7f4ff1b22d8f639dbedc2b564f9c6e397296
riyajidevindu <riyajidevindu@gmail.com>
2025-11-12 10:30:24 +0530

    Add auth-service module skeleton

--- backend/auth-service/src/main/java/com/bookfair/auth_service/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/bookfair/auth_service/AuthServiceApplication.java
@@ -1,9 +1,14 @@
 package com.bookfair.auth_service;
 
+import com.bookfair.auth_service.config.JwtProperties;
+import com.bookfair.auth_service.config.KafkaProperties;
+import com.bookfair.auth_service.config.SecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({JwtProperties.class, SecurityProperties.class, KafkaProperties.class})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
