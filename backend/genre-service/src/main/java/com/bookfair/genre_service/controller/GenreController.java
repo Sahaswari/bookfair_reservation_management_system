1f9baf81780abf38f99dd3ba9c96ad31b71afc53
Sahaswari Senanayaka <smsahaswari@gmail.com>
2025-11-25 15:13:59 +0530

    Update request mapping for GenreController to include API prefix

--- backend/genre-service/src/main/java/com/bookfair/genre_service/controller/GenreController.java
+++ backend/genre-service/src/main/java/com/bookfair/genre_service/controller/GenreController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/genres")
+@RequestMapping("api/genres")
 public class GenreController {
 
     private final GenreService genreService;
